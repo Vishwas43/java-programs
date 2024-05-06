30afeb6e1331392eddb892f15d103744cdb41a9f
Vishwas43 <160921251+Vishwas43@users.noreply.github.com>
2024-05-06 09:57:09 +0530

    Add files via upload

---
+++ rocky/Arthmetic.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class Arthmetic {
+
+public static void main (String[] args) {
+
+// Arithmetic operators
+
+int a=10;
+int b=3;
+
+System.out.println("a+b="+(a+b));
+System.out.println("a-b="+(a-b));
+System.out.println("a*b="+(a*b));
+System.out.println("a/b="+(a/b));
+System.out.println("a%b="+(a%b));
+
+}
+
+}
